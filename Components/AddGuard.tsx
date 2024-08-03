1c6957eb7418d9c44fa7118ae1319884b72bab8b
Muhammad Ali <alihere786@gmail.com>
2024-08-03 15:00:50 +0500

    Firebase Storage and Retrival

--- Components/AddGuard.tsx
+++ Components/AddGuard.tsx
@@ -1,47 +1,186 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, TextInput, Alert, FlatList } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import firestore from '@react-native-firebase/firestore';
+import PrimaryButton from './PrimaryButton';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 const AddGuard = () => {
-    const [AllData, SetData] = useState<any | null>(null);  // Initialize state as null
+
+    const [GuardName, SetguardName] = useState<any | null>(null);
+    const [FatherName, SetFatherName] = useState<any | null>(null);
+    const [CNIC, SetCNIC] = useState<any | null>(null);
+    const [Address, SetAddress] = useState<any | null>(null);
+    const [Salary, SetSalary] = useState<any | null>(null);
+    const [Phone, Setphone] = useState<any | null>(null);
+
+
+    const [GuardsData, SetGuardData] = useState<any[]>([]); // Initialize as an array
+
 
     useEffect(() => {
         GetDatabase();
     }, []);
 
+
+
+    const HandleAddData = async () => {
+        try {
+            await firestore().
+                collection("Add_Guard_Collection").
+                add({
+                    GName: GuardName,
+                    GFName: FatherName,
+                    GCNIC: CNIC,
+                    GAddress: Address,
+                    GSalary: Salary,
+                    GPhone: Phone,
+                }).
+                then(() => {
+                    console.log('User added!');
+                });
+
+            Alert.alert("Guard Added to Firebase!");
+
+            SetguardName("");
+            SetFatherName("");
+            SetCNIC("");
+            SetAddress("");
+            SetSalary("");
+            Setphone("");
+
+            console.log("NEW DATA ADDED!");
+            
+
+            GetDatabase(); // Refresh data
+
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
+
     const GetDatabase = async () => {
         try {
-            const Data = await firestore().collection("HelloDatabase").doc("userDataTable").get();
-            console.log(Data.data());
-            SetData(Data.data() || null);  // Set null if data is not available
+            const snapshot = await firestore().collection('Add_Guard_Collection').get();
+            const gaurdsdata = snapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+
+            console.log(gaurdsdata);
+            SetGuardData(gaurdsdata);
         } catch (error) {
             console.log(error);
         }
     };
 
     return (
-        <View>
-            <Text style={styles.textstyle}>
-                Name: {AllData ? AllData.Name : "Loading"}
-            </Text>
-            <Text style={styles.textstyle}>
-                Age: {AllData ? AllData.Age : "Loading"}
-            </Text>
-            <Text style={styles.textstyle}>
-                Graduated: {AllData === null ? "Loading" : (AllData.IsGraduated ? "True" : "No")}
-            </Text>
-            <Text style={styles.textstyle}>
-                Hobbies: {AllData ? (AllData.Hobbies ? AllData.Hobbies.join(', ') : 'No hobbies available') : 'Loading'}
-            </Text>
-        </View>
+        <SafeAreaView style={styles.loginPageContainer}>
+            <View style={styles.textInputContainer}>
+                <View style={styles.lockiconContainer}>
+                    <Icon name="user" size={30} color="black" style={styles.icon} />
+                </View>
+                <TextInput style={styles.textInputfeild} placeholder="Name" onChangeText={(value) => { SetguardName(value) }} value={GuardName}></TextInput>
+            </View>
+            <View style={styles.textInputContainer}>
+                <View style={styles.lockiconContainer}>
+                    <Icon name="user" size={30} color="black" style={styles.icon} />
+                </View>
+                <TextInput style={styles.textInputfeild} placeholder="Father Name" onChangeText={(value) => { SetFatherName(value) }} value={FatherName}></TextInput>
+            </View>
+
+            <View style={styles.textInputContainer}>
+                <View style={styles.iconContainer}>
+                    <Icon name="vcard" size={26} color="black" style={styles.icon} />
+                </View>
+                <TextInput style={styles.textInputfeild} placeholder="CNIC" onChangeText={(value) => { SetCNIC(value) }} value={CNIC}></TextInput>
+            </View>
+
+
+            <View style={styles.textInputContainer}>
+                <View style={styles.iconContainer}>
+                    <Icon name="home" size={30} color="black" style={styles.icon} />
+                </View>
+                <TextInput style={styles.textInputfeild} placeholder="Address" onChangeText={(value) => { SetAddress(value) }} value={Address}></TextInput>
+            </View>
+
+            <View style={styles.textInputContainer}>
+                <View style={styles.lockiconContainer}>
+                    <Icon name="dollar" size={31} color="black" style={styles.icon} />
+                </View>
+                <TextInput style={styles.textInputfeild} placeholder="Salary (PKR)" onChangeText={(value) => { SetSalary(value) }} value={Salary}></TextInput>
+            </View>
+
+            <View style={styles.textInputContainer}>
+                <View style={styles.lockiconContainer}>
+                    <Icon name="phone" size={25} color="black" style={styles.icon} />
+                </View>
+                <TextInput style={styles.textInputfeild} placeholder="Phone" onChangeText={(value) => { Setphone(value) }} value={Phone}></TextInput>
+            </View>
+
+            <View style={styles.buttonContainer}>
+                <PrimaryButton onPress={HandleAddData} text="Sign Up" color="black" textcolor="white" ></PrimaryButton>
+            </View>
+            
+        </SafeAreaView>
+
+
+
     );
 };
 
 const styles = StyleSheet.create({
-    textstyle: {
-        fontSize: 20,
+    loginPageContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingHorizontal: 40,
+        // backgroundColor: "lightblue"
+
+    },
+    textInputContainer: {
+        backgroundColor: "lightgrey",
+        width: "100%",
+        height: 50,
+        flexDirection: "row",
+        borderRadius: 6,
+        marginBottom: 15,
+    },
+    textInputfeild: {
+        width: "90%",
+        paddingHorizontal: 10,
+        paddingVertical: 7,
+        fontSize: 16,
         color: "black",
     },
-});
+    icon: {
+        borderRightColor: "black",
+        borderRightWidth: 2,
+        paddingRight: 15,
+        marginLeft: 10,
+    },
+    iconContainer: {
+        marginLeft: 2,
+        padding: 5,
+        alignContent: "center",
+        justifyContent: "center",
 
+    },
+    lockiconContainer: {
+        padding: 8,
+        alignContent: "center",
+        justifyContent: "center",
+        marginLeft: 6,
+
+    },
+    buttonContainer: {
+        marginTop: 30,
+        width: "100%"
+    },
+    
+
+
+});
 export default AddGuard;
--- Components/GuardHomePage.tsx
+++ Components/GuardHomePage.tsx
@@ -1,63 +1,110 @@
-import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView, TextInput } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
 import '../gesture-handler';
-import React from 'react';
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
+import React, { useEffect, useState } from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Icon from 'react-native-vector-icons/FontAwesome';
-// import AddGuard from './AddGuard';
-import PrimaryButton from "../Components/PrimaryButton";
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import firestore from '@react-native-firebase/firestore';
+import { useFocusEffect } from '@react-navigation/native';
 
 const Drawer = createDrawerNavigator();
 
 type RootStackParamList = {
-    Login: undefined;
-    GuardHome: undefined;
-    AddGuard: undefined;
-  };
-  
-  type GuardHomePageProps = NativeStackScreenProps<RootStackParamList, 'GuardHome'>;
+  Login: undefined;
+  GuardHome: undefined;
+  AddGuard: undefined;
+};
 
+type GuardHomePageProps = NativeStackScreenProps<RootStackParamList, 'GuardHome'>;
 
-const GuardHomePage = ({navigation}: GuardHomePageProps) => {
+const GuardHomePage = ({ navigation }: GuardHomePageProps) => {
+  function handleChangeScreen() {
+    navigation.navigate("AddGuard");
+  }
 
-    function handleChangeScreen() {
-        navigation.navigate("AddGuard");
-    }
-
-    return (
-        <Drawer.Navigator>
-            <Drawer.Screen
-                name="Guard"
-                component={GuardPage}
-                options={{
-                    headerStyle: {
-                         backgroundColor: 'black',
-                    },
-                    headerTintColor: 'white',
-                    headerTitleAlign: "center",
-                    headerRight: () => (
-                        <TouchableOpacity onPress={handleChangeScreen}>
-                            <Icon name="plus-circle" size={30} color="white" style={{marginRight: 15}} />
-                        </TouchableOpacity>
-                    ),
-                }}
-            />
-            <Drawer.Screen name="Customer" component={GuardPage} />
-            <Drawer.Screen name="Salaries" component={GuardPage} />
-            <Drawer.Screen name="Collection" component={GuardPage} />
-        </Drawer.Navigator>
-    );
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen
+        name="Guard"
+        component={GuardPage}
+        options={{
+          headerStyle: {
+            backgroundColor: 'black',
+          },
+          headerTintColor: 'white',
+          headerTitleAlign: "center",
+          headerRight: () => (
+            <TouchableOpacity onPress={handleChangeScreen}>
+              <Icon name="plus-circle" size={30} color="white" style={{ marginRight: 15 }} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Drawer.Screen name="Customer" component={GuardPage} />
+      <Drawer.Screen name="Salaries" component={GuardPage} />
+      <Drawer.Screen name="Collection" component={GuardPage} />
+    </Drawer.Navigator>
+  );
 };
 
 export default GuardHomePage;
 
 function GuardPage() {
-    return (
-        <View>
-            <Text>Guard Page</Text>
-        </View>
-    );
+  const [GuardsData, SetGuardData] = useState<any[]>([]); // Initialize as an array
+
+  useEffect(() => {
+    const subscriber = firestore().collection('Add_Guard_Collection').onSnapshot(querySnapshot => {
+      const guardsData = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+
+      console.log(guardsData);
+      SetGuardData(guardsData);
+    }, error => {
+      console.log(error);
+    });
+
+    // Unsubscribe from events when no longer in use
+    return () => subscriber();
+  }, []);
+
+  return (
+    <View style={styles.mainContainer}>
+      <FlatList
+        data={GuardsData}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.listcontainer}>
+            <Text style={styles.cardText}>Name: {item.GName}</Text>
+            <Text style={styles.cardTextFather}>Father Name: {item.GFName}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
 }
 
-
-
+const styles = StyleSheet.create({
+  mainContainer: {
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    backgroundColor: "#ececec",
+    flex: 1,
+  },
+  listcontainer: {
+    marginTop: 10,
+    backgroundColor: "#ffffff",
+    padding: 15,
+    width: "100%",
+    borderRadius: 8,
+  },
+  cardText: {
+    fontSize: 25,
+    color: "black"
+  },
+  cardTextFather: {
+    fontSize: 20,
+    color: "black"
+  },
+});
