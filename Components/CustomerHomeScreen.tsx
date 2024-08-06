87c4a2f298cc85ab86023431187622c6e140d9d6
Muhammad Ali <alihere786@gmail.com>
2024-08-07 01:39:41 +0500

    modified:   Components/CustomerHomeScreen.tsx

--- Components/CustomerHomeScreen.tsx
+++ Components/CustomerHomeScreen.tsx
@@ -1,8 +1,9 @@
-import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, FlatList, Modal } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import firestore from '@react-native-firebase/firestore';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import PrimaryButton from './PrimaryButton';
 
 type RootStackParamList = {
     CustomerHomeScreen: undefined;
@@ -11,13 +12,12 @@ type RootStackParamList = {
 
 type DetailsScreenProps = NativeStackScreenProps<RootStackParamList, 'CustomerHomeScreen'>;
 
-
 const CustomerHomeScreen = ({ navigation }: DetailsScreenProps) => {
 
     const [CustomersData, SetCustomersData] = useState<any[]>([]);
+    const [IsModalVisible, SetIsModalVisible] = useState(false);
 
     useEffect(() => {
-
         try {
             const subscriber = firestore()
                 .collection('Add_Customer_Collection')
@@ -38,14 +38,13 @@ const CustomerHomeScreen = ({ navigation }: DetailsScreenProps) => {
             console.log(error);
         }
 
-    }, [])
+    }, []);
 
     const HandleCustomerDetails = (CustomerID: string) => {
         console.log("Pressed!");
 
-        navigation.navigate("CustomerDetails", { CustomerID })
+        navigation.navigate("CustomerDetails", { CustomerID });
     }
-
 
     return (
         <View style={styles.mainContainer}>
@@ -54,23 +53,37 @@ const CustomerHomeScreen = ({ navigation }: DetailsScreenProps) => {
                 keyExtractor={(item) => item.id}
                 renderItem={({ item }) => (
                     <View style={styles.listcontainer}>
-                        <TouchableOpacity onPress={() => { HandleCustomerDetails(item.id) }}>
-                            <View style={styles.dataSide}>
-                                <Text style={styles.cardText}>ID: {item ? item.id : "Loading"}</Text>
-                                <Text style={styles.cardText}>Customer: {item ? item.CName : "Loading"}</Text>
+                        <View style={{ flexDirection: "row", flex: 1 }}>
+                            <View style={styles.dataSide} >
+                                <TouchableOpacity onPress={() => { HandleCustomerDetails(item.id) }}>
+                                    <Text style={styles.cardText}>ID: {item ? item.id : "Loading"}</Text>
+                                    <Text style={styles.cardText}>Customer: {item ? item.CName : "Loading"}</Text>
+                                    <Text style={styles.cardText}>Customer: {item ? item.CName : "Loading"}</Text>
+                                </TouchableOpacity>
                             </View>
-                        </TouchableOpacity>
-                        <View style={styles.iconSide}>
-                            <Icon name="ellipsis-v" size={30} color="#000000" style={{ marginRight: 15 }} />
+
+                            <View style={styles.iconView}>
+                                <TouchableOpacity onPress={() => { SetIsModalVisible(true) }}>
+                                    <Icon name="ellipsis-v" size={42} color="#000000" />
+                                </TouchableOpacity>
+                            </View>
                         </View>
                     </View>
                 )}
             />
+            <Modal visible={IsModalVisible} transparent={true} animationType='slide'>
+                <View style={styles.modalBackground}>
+                    <View style={styles.modalcontainer}>
+                        <Text style={styles.cardText}>This is Modal</Text>
+                        <PrimaryButton text='Back' onPress={() => { SetIsModalVisible(false) }} />
+                    </View>
+                </View>
+            </Modal>
         </View>
-    )
+    );
 }
 
-export default CustomerHomeScreen
+export default CustomerHomeScreen;
 
 const styles = StyleSheet.create({
     mainContainer: {
@@ -80,29 +93,42 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     listcontainer: {
+        borderRadius: 10,
         marginTop: 10,
-        backgroundColor: "white",
-        height: 80,
+        backgroundColor: "#ffffff",
+        height: 120,
         width: "100%",
-        borderRadius: 8,
-        flexDirection: "row"
+        overflow: "hidden"
+    },
+    modalBackground: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.1)'  // Semi-transparent background
+    },
+    modalcontainer: {
+        backgroundColor: "white",
+        width: 300,
+        height: 200,
+        borderRadius: 10,
+        alignSelf: 'center',
+        margin: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        elevation: 5,  // Optional: adds shadow effect
     },
     cardText: {
         fontSize: 18,
         color: "black"
     },
-    cardTextFather: {
-        fontSize: 16,
-        color: "black"
-    },
     dataSide: {
-        flex: 7,
-        padding: 15,
+        flex: 8,
+        justifyContent: "center",
+        paddingHorizontal: 20,
     },
-    iconSide: {
-        flex: 1,
+    iconView: {
+        flex: 2,
         justifyContent: "center",
         alignItems: "center",
-        paddingLeft: 15,
     }
 });
