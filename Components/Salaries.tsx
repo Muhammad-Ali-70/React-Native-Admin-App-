755fac4a3aba9d95531ffd10aa687f1b60941d4b
Muhammad Ali <alihere786@gmail.com>
2024-08-09 22:15:36 +0500

    modified:   Components/Salaries.tsx

---
+++ Components/Salaries.tsx
@@ -0,0 +1,135 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
+const data = [
+    { label: 'Item 1', value: '1' },
+    { label: 'Item 2', value: '2' },
+    { label: 'Item 3', value: '3' },
+    { label: 'Item 4', value: '4' },
+    { label: 'Item 5', value: '5' },
+    { label: 'Item 6', value: '6' },
+    { label: 'Item 7', value: '7' },
+    { label: 'Item 8', value: '8' },
+];
+
+// const Salaries = () => {
+
+
+//     return (
+//         <View style={styles.mainContainer}>
+//             <Text style={styles.headtext}>Summary</Text>
+//             <TextInput style={styles.dropdown} placeholder='This is Drop Down Box'></TextInput>
+//         </View>
+//     )
+// }
+
+// export default Salaries
+
+
+// const styles = StyleSheet.create({
+//     mainContainer: {
+//         flex: 1,
+//         alignItems: "center",
+//         justifyContent: "center",
+//     },
+//     headtext: {
+
+//     },
+//     dropdown: {
+//         fontSize: 18,
+//         width: "70%",
+//         height: 45,
+//         borderColor: "black",
+//         borderWidth: 1,
+//         borderRadius: 10,
+
+//     }
+// })
+
+const Salaries = () => {
+
+    const [value, setValue] = useState(null);
+    const [isFocus, setIsFocus] = useState(false);
+
+
+    return (
+        <View >
+            <Text style={styles.headtext}>Summary</Text>
+
+            <Dropdown
+                style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
+                placeholderStyle={styles.placeholderStyle}
+                selectedTextStyle={styles.selectedTextStyle}
+                inputSearchStyle={styles.inputSearchStyle}
+                iconStyle={styles.iconStyle}
+                data={data}
+                search
+                maxHeight={300}
+                labelField="label"
+                valueField="value"
+                placeholder={!isFocus ? 'Select item' : '...'}
+                searchPlaceholder="Search..."
+                value={value}
+                onFocus={() => setIsFocus(true)}
+                onBlur={() => setIsFocus(false)}
+                onChange={item => {
+                    setValue(item.value);
+                    setIsFocus(false);
+                }}
+                renderLeftIcon={() => (
+                    <Icon
+                        style={styles.icon}
+                        color={isFocus ? 'blue' : 'black'}
+                        name="retweet"
+                        size={20}
+                    />
+                )}
+            />
+        </View>
+    );
+};
+
+export default Salaries;
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    headtext: {
+        fontSize: 30,
+        fontStyle: "normal",
+        fontWeight: "bold",
+        color: "black",
+    },
+    dropdown: {
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        borderRadius: 8,
+        paddingHorizontal: 8,
+    },
+    icon: {
+        padding: 10,
+    },
+    placeholderStyle: {
+        // color: "black",
+        fontSize: 16,
+    },
+    selectedTextStyle: {
+        color: "black",
+        fontSize: 16,
+    },
+    iconStyle: {
+        width: 20,
+        height: 20,
+    },
+    inputSearchStyle: {
+        height: 40,
+        fontSize: 16,
+    },
+});
